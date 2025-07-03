7a1ce7080964c10ed5fe28f886c932b4a872cdf6
rilmayer <yessenbayev.chingis@gmail.com>
2025-07-03 08:55:23 +0500

    LCODE-3304: solution

---
+++ src/FindTheKthCharacterInString.java
@@ -0,0 +1,22 @@
+public class FindTheKthCharacterInString {
+    public static char kthCharacter(int k) {
+        StringBuilder word = new StringBuilder("a");
+
+        while (word.length() < k) {
+            word.append(next(word));
+        }
+        return word.charAt(k - 1);
+    }
+
+    private static String next(StringBuilder word) {
+        StringBuilder result = new StringBuilder();
+        for (char ch : word.toString().toCharArray()) {
+            result.append((char) ((ch - 'a' + 1) % 26 + 'a'));
+        }
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(kthCharacter(10));
+    }
+}
