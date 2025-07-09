c8b07724d0f2ea81dc82a8a73fed5f47c1c0e958
rilmayer <yessenbayev.chingis@gmail.com>
2025-07-09 07:30:29 +0500

    LCODE-3439: solution

---
+++ src/MaxFreeTime.java
@@ -0,0 +1,21 @@
+public class MaxFreeTime {
+    public int maxFreeTime(int eventTime, int k, int[] startTime, int[] endTime) {
+        int n = startTime.length;
+        int[] gaps = new int[n + 1];
+        gaps[0] = startTime[0];
+        for (int i = 1; i < n; i++) {
+            gaps[i] = startTime[i] - endTime[i - 1];
+        }
+        gaps[n] = eventTime - endTime[n - 1];
+
+        int ans = 0, sum = 0;
+        for (int i = 0; i <= n; i++) {
+            sum += gaps[i];
+            if (i >= k) {
+                ans = Math.max(ans, sum);
+                sum -= gaps[i - k];
+            }
+        }
+        return ans;
+    }
+}
