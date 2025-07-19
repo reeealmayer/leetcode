30242170d798c4c1dc888f844939ba6ea2c063d8
rilmayer <yessenbayev.chingis@gmail.com>
2025-07-19 07:23:42 +0500

    LCODE-1233: solution

---
+++ src/medium/RemoveSubfoldersFromTheFileSystem.java
@@ -0,0 +1,21 @@
+package medium;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class RemoveSubfoldersFromTheFileSystem {
+    public List<String> removeSubfolders(String[] folder) {
+        Arrays.sort(folder);
+        List<String> result = new ArrayList<>();
+
+        for (String path : folder) {
+            // Если result пуст или текущий путь не под-папка последнего в result
+            if (result.isEmpty() || !path.startsWith(result.get(result.size() - 1) + "/")) {
+                result.add(path);
+            }
+        }
+
+        return result;
+    }
+}
