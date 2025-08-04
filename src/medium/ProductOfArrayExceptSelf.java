3e4d7d70435c8278a4eebb22e470630e0133ccde
rilmayer <yessenbayev.chingis@gmail.com>
2025-08-04 18:18:27 +0500

    LCODE-238: solution

---
+++ src/medium/ProductOfArrayExceptSelf.java
@@ -0,0 +1,21 @@
+package medium;
+
+public class ProductOfArrayExceptSelf {
+    public int[] productExceptSelf(int[] nums) {
+        int length = nums.length;
+        int[] result = new int[length];
+
+        result[0] = 1;
+        for (int i = 1; i < length; i++) {
+            result[i] = result[i - 1] * nums[i - 1];
+        }
+
+        int right = 1;
+        for (int i = length - 1; i >= 0; i--) {
+            result[i] = result[i] * right;
+            right *= nums[i];
+        }
+
+        return result;
+    }
+}
