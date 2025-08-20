8865320fbe6f1dd65c4aecc69f1fe5caa52e68fc
rilmayer <yessenbayev.chingis@gmail.com>
2025-08-20 08:28:44 +0500

    LCODE-1277: solution

---
+++ src/medium/ContainerWithMostWater11.java
@@ -0,0 +1,28 @@
+package medium;
+
+public class ContainerWithMostWater11 {
+    public static int maxArea(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int maxArea = 0;
+
+        while (left < right) {
+            int width = right - left;
+            int h = Math.min(height[left], height[right]);
+            int area = width * h;
+            maxArea = Math.max(maxArea, area);
+
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return maxArea;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(maxArea(new int[]{1, 8, 6, 2, 5, 4, 8, 3, 7}));
+    }
+}
