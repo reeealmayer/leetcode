976eaa4ade204586404530f0fc5b7842bd7b7509
rilmayer <yessenbayev.chingis@gmail.com>
2025-09-02 08:29:43 +0500

    LCODE-704: solution

--- src/easy/BinarySearch704.java
+++ src/easy/BinarySearch704.java
@@ -1,17 +1,15 @@
-package not_sorted;
+package easy;
 
-public class SearchInsertPosition {
-    public static int searchInsert(int[] nums, int target) {
+public class BinarySearch704 {
+    public static int search(int[] nums, int target) {
         int left = 0;
         int right = nums.length - 1;
 
         while (left <= right) {
             int mid = (left + right) / 2;
-
-            if (target == nums[mid]) {
+            if (nums[mid] == target) {
                 return mid;
             }
-
             if (nums[mid] > target) {
                 right = mid - 1;
             } else {
@@ -19,10 +17,11 @@ public class SearchInsertPosition {
             }
         }
 
-        return left;
+        return -1;
     }
 
     public static void main(String[] args) {
-        System.out.println(searchInsert(new int[]{1, 2, 5, 7, 9}, 3));
+        int[] arr = new int[]{-1, 0, 3, 5, 9, 12};
+        System.out.println(search(arr, 12));
     }
 }
