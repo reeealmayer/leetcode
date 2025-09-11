51d4c97d88f5ccdcf46e4cc5f5fb644fb92591ff
rilmayer <yessenbayev.chingis@gmail.com>
2025-09-11 07:14:26 +0500

    LCODE-206: solution

---
+++ src/medium/ReverseLinkedList206.java
@@ -0,0 +1,19 @@
+package medium;
+
+import not_sorted.ListNode;
+
+public class ReverseLinkedList206 {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+        }
+
+        return prev;
+    }
+}
+
--- src/not_sorted/ListNode.java
+++ src/not_sorted/ListNode.java
@@ -1,8 +1,8 @@
 package not_sorted;
 
 public class ListNode {
-    int val;
-    ListNode next;
+    public int val;
+    public ListNode next;
 
     ListNode() {
     }
