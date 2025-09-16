9bbcd3555b9a4e5207eadcca112631c245833bcf
rilmayer <yessenbayev.chingis@gmail.com>
2025-09-16 07:47:31 +0500

    LCODE-143: solution

--- src/medium/ReorderList143.java
+++ src/medium/ReorderList143.java
@@ -4,28 +4,27 @@ import not_sorted.ListNode;
 
 public class ReorderList143 {
     public static void reorderList(ListNode head) {
-        ListNode dummy = head;
-        ListNode slow = dummy;
-        ListNode fast = dummy;
-
+        ListNode slow = head;
+        ListNode fast = head;
         while (fast != null && fast.next != null) {
             slow = slow.next;
             fast = fast.next.next;
         }
-
         ListNode prev = null;
         ListNode curr = slow.next;
         slow.next = null;
-        while(curr != null) {
+        while (curr != null) {
             ListNode temp = curr.next;
             curr.next = prev;
             prev = curr;
             curr = temp;
         }
 
+        // slow
+        // prev
+
         ListNode first = head;
         ListNode second = prev;
-
         while (second != null) {
             ListNode temp1 = first.next;
             ListNode temp2 = second.next;
@@ -47,7 +46,14 @@ public class ReorderList143 {
         node2.next = node3;
         ListNode node4 = new ListNode(4);
         node3.next = node4;
+        ListNode node5 = new ListNode(5);
+        node4.next = node5;
 
         reorderList(node1);
+
+        System.out.println(node1.val);
+        System.out.println(node1.next.val);
+        System.out.println(node1.next.next.val);
+        System.out.println(node1.next.next.next.val);
     }
 }
