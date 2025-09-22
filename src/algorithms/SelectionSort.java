922a02da55992ef3b7506883ecce2e7b86a555b9
rilmayer <yessenbayev.chingis@gmail.com>
2025-09-22 10:10:25 +0500

    LCODE-no: selection sort

---
+++ src/algorithms/SelectionSort.java
@@ -0,0 +1,28 @@
+package algorithms;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {5, 4, 3, 2, 1};
+        selectionSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
