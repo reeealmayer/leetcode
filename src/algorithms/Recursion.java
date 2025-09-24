8fba1741b45fe422b8695fd27a819d17c50b8310
rilmayer <yessenbayev.chingis@gmail.com>
2025-09-24 10:12:12 +0500

    LCODE-notask: recursion quick sort

--- src/algorithms/Recursion.java
+++ src/algorithms/Recursion.java
@@ -1,5 +1,7 @@
 package algorithms;
 
+import java.util.Arrays;
+
 public class Recursion {
 
     public static void countDown(int n) {
@@ -54,10 +56,42 @@ public class Recursion {
         }
     }
 
+    public static void quickSort(int[] arr, int left, int right) {
+        if (left < right) {
+            int pivotIndex = partition(arr, left, right);
+
+            quickSort(arr, left, pivotIndex - 1);
+            quickSort(arr, pivotIndex + 1, right);
+        }
+    }
+
+    private static int partition(int[] arr, int left, int right) {
+        int pivot = arr[right];
+        int i = left - 1;
+        for (int j = left; j < right; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+                swap(arr, i, j);
+            }
+        }
+        swap(arr, i + 1, right);
+        return i + 1;
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
 
     public static void main(String[] args) {
         countDown(10);
         System.out.println();
         System.out.println(factorial(5));
+
+        int[] nums = {10, 7, 8, 9, 1, 5};
+        quickSort(nums, 0, nums.length - 1);
+        System.out.println(Arrays.toString(nums));
     }
 }
