32c60f80abbe4a09e2169335f025a972561f89e6
rilmayer <yessenbayev.chingis@gmail.com>
2025-09-25 16:48:41 +0500

    LCODE-120: my own solution

--- src/medium/Triangle120.java
+++ src/medium/Triangle120.java
@@ -14,14 +14,11 @@ public class Triangle120 {
             dp[i] = triangle.get(n - 1).get(i);
         }
 
-
         for (int i = n - 2; i >= 0; i--) {
             for (int j = 0; j <= i; j++) {
                 dp[j] = triangle.get(i).get(j) + Math.min(dp[j], dp[j + 1]);
             }
         }
-
-
         return dp[0];
     }
 
