11a4a5d05973f225835d3e63a8126f5f9220e0e4
rilmayer <yessenbayev.chingis@gmail.com>
2025-10-01 14:57:21 +0500

    LCODE-1518: solution

---
+++ src/easy/WaterBottles1518.java
@@ -0,0 +1,19 @@
+package easy;
+
+public class WaterBottles1518 {
+    public int numWaterBottles(int numBottles, int numExchange) {
+        int consumedBottles = 0;
+
+        while (numBottles >= numExchange) {
+            // Consume numExchange full bottles.
+            consumedBottles += numExchange;
+            numBottles -= numExchange;
+
+            // Exchange them for one full bottle.
+            numBottles++;
+        }
+
+        // Consume the remaining numBottles (<numExchange).
+        return consumedBottles + numBottles;
+    }
+}
